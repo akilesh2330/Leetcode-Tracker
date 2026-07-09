037f29ed350d7c945617c5d8c518dd6ea60d3b76
akilesh2330 <akilesh2330@gmail.com>
2026-07-09 15:09:26 +0530

    Create 602-FriendRequestsIiWhoHasTheMostFriends

---
+++ 602-FriendRequestsIiWhoHasTheMostFriends/602-FriendRequestsIiWhoHasTheMostFriends.sql
@@ -0,0 +1,14 @@
+-- Last updated: 7/9/2026, 3:09:26 PM
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+) AS Friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
