660033cebb670880b7531975d183b0ce0528d2c2
akilesh2330 <akilesh2330@gmail.com>
2026-07-09 15:07:56 +0530

    Create 3515-FindIfDigitGameCanBeWon

---
+++ 3515-FindIfDigitGameCanBeWon/3515-FindIfDigitGameCanBeWon.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 3:07:55 PM
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int sd=0;
+        int dd=0;
+        for(int x:nums){
+            if(x<10) sd+=x;
+            else dd+=x;
+        }
+        return sd!=dd;
+    }
+};
