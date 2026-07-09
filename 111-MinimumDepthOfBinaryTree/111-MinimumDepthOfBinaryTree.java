8f8ad447b2e2f149f5d4b28efe53734fd64a241d
akilesh2330 <akilesh2330@gmail.com>
2026-07-09 15:11:19 +0530

    Create 111-MinimumDepthOfBinaryTree

---
+++ 111-MinimumDepthOfBinaryTree/111-MinimumDepthOfBinaryTree.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:11:18 PM
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
